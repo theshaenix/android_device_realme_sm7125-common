82c113cf3861007a107de8433278fcd2165fa87f
Michael Bestas <mkbestas@lineageos.org>
2023-11-22 19:21:47 +0200

    sm7125-common: Build recovery QTI health AIDL service
    We have a recovery partition.
    
    Change-Id: I10841b26feea3c214349a20b2eedec74924f993a

--- common.mk
+++ common.mk
@@ -184,7 +184,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
