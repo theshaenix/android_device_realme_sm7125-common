4e9d94cf2a4d1fd4466d75027c411d2dafc6c9dc
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    sm7125-common: Allow to access bootable/deprecated-ota targets
    Sm7125-common is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm6150-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- common.mk
+++ common.mk
@@ -407,6 +407,7 @@ PRODUCT_SOONG_NAMESPACES += \
     kernel/realme/sm7125 \
     hardware/google/interfaces \
     hardware/google/pixel \
+    bootable/deprecated-ota \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
     hardware/qcom-caf/common/libqti-perfd-client
