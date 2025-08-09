e90a9f8a1ffdf74bd7e043b30ff5d9a988472a62
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    sm7125-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,7 +101,6 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
     $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
