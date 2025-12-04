f14aeab185ea3f8ac92443d5a784c2bfe419165d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-03 22:30:26 -0500

    sm7125-common: Update some soong config variables to bool type

--- common.mk
+++ common.mk
@@ -270,7 +270,7 @@ $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
 
-$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
 
 # Media
 PRODUCT_PACKAGES += \
