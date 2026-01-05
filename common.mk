0833d13096782d64b8fe2c8e1cd8a8cc670c0083
theshaenix <theshaenix@gmail.com>
2026-01-06 01:13:30 +0530

    sm7125-common: switch to libprotobuf-cpp-lite-3.9.1-vendorcompat

--- common.mk
+++ common.mk
@@ -357,8 +357,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 #Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1
-
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    
 # QTI FWK Detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
